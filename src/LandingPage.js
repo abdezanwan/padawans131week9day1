ede40df4f88f242f96a4551bb995604cae329a73
Rahiem Adam <abdezanwan@gmail.com>
2023-10-24 13:52:42 -0500

    W9-D1

---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple landing page.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
